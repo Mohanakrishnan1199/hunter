4552803f3c9ef0ae49633dd6aac0a0b40595a319
Mohanakrishnan1199 <51222571+Mohanakrishnan1199@users.noreply.github.com>
2019-08-11 11:43:44 +0530

    Update hunter 7.py

--- hunter 7.py
+++ hunter 7.py
@@ -1,28 +1,7 @@
 m=int(input())
-mo=list(map(int,input().split()[:m]))
-d=[]
-for i in mo:
-    if mo.count(i)>1:
-        h=i
-        for j in range(0,len(mo)):
-            if mo[j]==h:
-                if j%2==0:
-                    if h%2!=0:
-                        if h not in d:
-                            d.append(h)
-                        
-            else:
-                if h%2==0:
-                    if h not in d:
-                        d.append(h)
-    else:
-        k=mo.index(i)
-        if k%2==0:
-            if i%2!=0:
-                if i not in d:
-                    d.append(i)
-        else:
-            if i%2==0:
-                if i not in d:
-                    d.append(i)
-print(*d)
+mo=list(map(int,input().split()))[:m]
+k=[]
+for i in range(0,m):
+  if(mo[i]%2==0 and i%2==1 or mo[i]%2==1 and i%2==0):
+    k.append(mo[i])
+print(*k,end="")
